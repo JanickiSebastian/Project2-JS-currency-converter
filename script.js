fe66fe18a46edd85f37ec860c28aa3df1c70e6e5
Sebastian Janicki <seba789@vp.pl>
2023-07-21 01:11:27 +0200

    project upgraded

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 const apiUrl = "https://api.nbp.pl/api/exchangerates/rates/a/";
 
-function showLoader() {
-    document.getElementById("loader").style.display = "block";
+function toggleLoader(show) {
+  const loader = document.getElementById("loader");
+  loader.style.display = show ? "block" : "none";
 }
-
-function hideLoader() {
-    document.getElementById("loader").style.display = "none";
-}
-
 async function getCurrencyRate(currency) {
-    const response = await fetch(`${apiUrl}${currency}`);
-    const data = await response.json();
-    return data.rates[0].mid;
+  const response = await fetch(`${apiUrl}${currency}`);
+  const data = await response.json();
+  return data.rates[0].mid;
 }
 
 function calculateResult(rate, amount) {
-    const result = rate * amount;
-    return result.toFixed(2);
+  const result = rate * amount;
+  return result.toFixed(2);
+}
+
+function isValidAmount(amount) {
+  return parseFloat(amount) >= 0.01;
 }
 
 async function convertCurrency() {
-    const currencySelect = document.getElementById("currency");
-    const selectedCurrency = currencySelect.value;
-    const amountInput = document.getElementById("amount");
-    const amount = amountInput.value;
+  const currencySelect = document.getElementById("currency");
+  const selectedCurrency = currencySelect.value;
+  const amountInput = document.getElementById("amount");
+  const amount = amountInput.value;
+  if (!isValidAmount(amount)) {
+    document.getElementById("result").innerText = "Błędna kwota. Wpisz liczbę większą lub równą 0.01.";
+    return;
+}
+toggleLoader(true);
+  try {
+    const rate = await getCurrencyRate(selectedCurrency);
+    const result = calculateResult(rate, amount);
 
-    showLoader();
+    document.getElementById(
+      "result"
+    ).innerText = `${amount} ${selectedCurrency} = ${result} PLN`;
+  } catch (error) {
+    document.getElementById("result").innerText =
+      "Błąd. Spróbuj ponownie później.";
+  }
 
-    try {
-        const rate = await getCurrencyRate(selectedCurrency);
-        const result = calculateResult(rate, amount);
-
-        document.getElementById("result").innerText = `${amount} ${selectedCurrency} = ${result} PLN`;
-    } catch (error) {
-        document.getElementById("result").innerText = "Błąd. Spróbuj ponownie później.";
-    }
-
-    hideLoader();
+  toggleLoader(false);
 }
 
-document.getElementById("convertBtn").addEventListener("click", convertCurrency);
+document
+  .getElementById("convertBtn")
+  .addEventListener("click", convertCurrency);
